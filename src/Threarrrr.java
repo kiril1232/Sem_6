1128e1a5d9e6ad7dc4bb6d8a330a64641394cc6c
Kiril <kirilsemenchuk600@gmail.com>
2023-03-19 15:57:02 +0000

    Edit Main And Threarrrr Delete cycle and Thread

--- src/Threarrrr.java
+++ src/Threarrrr.java
@@ -2,4 +2,10 @@
  * * Created by user on 19.03.2023
  */
 public class Threarrrr {
+    public static void main(String[] args) {
+        Runnable a = (Runnable) new Threarrrr();
+        Thread b = new Thread(a);
+        b.start();
+        // d.start
+    }
 }
